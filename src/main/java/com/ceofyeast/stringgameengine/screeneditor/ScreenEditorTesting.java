c9e9c1b91f57facdd81856d698400d456b49e8e8
CEOFYEAST <89040723+CEOFYEAST@users.noreply.github.com>
2024-01-05 11:42:22 -0600

    Added method to initialize a cell matrix given a face size, row count and column count

--- src/main/java/com/ceofyeast/stringgameengine/screeneditor/ScreenEditorTesting.java
+++ src/main/java/com/ceofyeast/stringgameengine/screeneditor/ScreenEditorTesting.java
@@ -4,17 +4,93 @@
  */
 package com.ceofyeast.stringgameengine.screeneditor;
 
+  // Used to enable full-screen mode
+import java.awt.GraphicsEnvironment;
+import java.awt.GraphicsDevice;
+
+  // Used to close window
+import java.awt.event.WindowEvent;
+
 /**
  *
  * @author bento
  */
 public class ScreenEditorTesting extends javax.swing.JFrame {
-
+  /**
+   * Base width of a cell in pixels
+   */
+  static final int BASE_WIDTH = 6;
+  
+  /**
+   * Base height of a cell in pixels
+   */
+  static final int BASE_HEIGHT = 13;
+  
+  /**
+   * Number of columns of cells in the screen
+   */
+  static int columnCount = 6;
+  
+  /**
+   * Number of rows of cells in the screen
+   */
+  static int rowCount = 3;
+  
+  /**
+   * Scale to apply to base ratio of 6x13 pixels, also to font-size
+   * Ex: Scale of two would result in cells with width 12 pixels and height 26 pixels (2 * 6, 13 )
+   */
+  static int faceSize = 12;
+  
+  /**
+   * Grid JPanel used to represent a Screen object graphically
+   */
+  javax.swing.JPanel cellsMatrix;
+  
+  /**
+   * Used to enable full-screen mode
+   */
+  static GraphicsDevice device = GraphicsEnvironment
+        .getLocalGraphicsEnvironment().getScreenDevices()[0];
+  
   /**
    * Creates new form ScreenEditorTesting
    */
   public ScreenEditorTesting() {
     initComponents();
+    
+    initializeCellsMatrix();
+    
+    device.setFullScreenWindow( this );
+  }
+  
+  public void initializeCellsMatrix() {
+    cellsMatrix = new javax.swing.JPanel();
+    
+    cellsMatrix.setLayout( new java.awt.GridLayout( rowCount, columnCount ) );
+    
+    for( int i = 0; i < rowCount * columnCount; i++ )
+    {
+      javax.swing.JTextField cellToAdd = new javax.swing.JTextField();
+      
+      cellToAdd.setHorizontalAlignment( javax.swing.JTextField.CENTER );
+      
+      cellToAdd.setText( "R" );
+      
+      cellToAdd.setBorder( javax.swing.BorderFactory.createLineBorder( new java.awt.Color( 0, 0, 0 ) ) );
+      
+      cellToAdd.setMargin( new java.awt.Insets(0, 0, 0, 0) );
+      
+      cellsMatrix.add( cellToAdd );
+    }
+    
+    getContentPane().add( cellsMatrix );
+    
+    int cellsMatrixWidth = BASE_WIDTH * faceSize * columnCount;
+    
+    int cellsMatrixHeight = BASE_HEIGHT * faceSize * rowCount;
+    
+    cellsMatrix.setBounds( 0, 0, cellsMatrixWidth, cellsMatrixHeight );
   }
 
   /**
@@ -26,21 +102,39 @@ public class ScreenEditorTesting extends javax.swing.JFrame {
   // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
   private void initComponents() {
 
-    setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+    jPanel1 = new javax.swing.JPanel();
+    menuBar = new javax.swing.JMenuBar();
+    fileMenu = new javax.swing.JMenu();
+    closeMenuItem = new javax.swing.JMenuItem();
 
-    javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-    getContentPane().setLayout(layout);
-    layout.setHorizontalGroup(
-      layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-      .addGap(0, 400, Short.MAX_VALUE)
-    );
-    layout.setVerticalGroup(
-      layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-      .addGap(0, 300, Short.MAX_VALUE)
-    );
+    setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+    getContentPane().setLayout(null);
+
+    jPanel1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+    getContentPane().add(jPanel1);
+    jPanel1.setBounds(90, 10, 100, 100);
+
+    fileMenu.setText("File");
+
+    closeMenuItem.setText("Close");
+    closeMenuItem.addActionListener(new java.awt.event.ActionListener() {
+      public void actionPerformed(java.awt.event.ActionEvent evt) {
+        closeActionPerformed(evt);
+      }
+    });
+    fileMenu.add(closeMenuItem);
+
+    menuBar.add(fileMenu);
+
+    setJMenuBar(menuBar);
 
     pack();
   }// </editor-fold>//GEN-END:initComponents
+
+  private void closeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_closeActionPerformed
+    // TODO add your handling code here:
+    this.dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+  }//GEN-LAST:event_closeActionPerformed
 
   /**
    * @param args the command line arguments
@@ -78,5 +172,9 @@ public class ScreenEditorTesting extends javax.swing.JFrame {
   }
 
   // Variables declaration - do not modify//GEN-BEGIN:variables
+  private javax.swing.JMenuItem closeMenuItem;
+  private javax.swing.JMenu fileMenu;
+  private javax.swing.JPanel jPanel1;
+  private javax.swing.JMenuBar menuBar;
   // End of variables declaration//GEN-END:variables
 }
