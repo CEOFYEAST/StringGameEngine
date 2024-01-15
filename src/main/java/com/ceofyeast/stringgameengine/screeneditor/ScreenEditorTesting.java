586230f2881547d62a29130b93f3f1df5401ec58
CEOFYEAST <89040723+CEOFYEAST@users.noreply.github.com>
2024-01-15 12:30:53 -0600

    Huge commit encapsulating CellsMatrix and Cell in their own subclasses. Also added extensive documentation comments.

--- src/main/java/com/ceofyeast/stringgameengine/screeneditor/ScreenEditorTesting.java
+++ src/main/java/com/ceofyeast/stringgameengine/screeneditor/ScreenEditorTesting.java
@@ -30,36 +30,199 @@ import java.io.File;
  */
 public class ScreenEditorTesting extends javax.swing.JFrame {
   /**
-   * Number of columns of cells in the screen
-   */
-  static int columnCount = 60;
-  
-  /**
-   * Number of rows of cells in the screen
-   */
-  static int rowCount = 18;
-  
-  /**
-   * Scale to apply to base ratio of 6x13 pixels, also to font-size
-   * Ex: Scale of two would result in cells with width 12 pixels and height 26 pixels (2 * 6, 13 )
-   */
-  static int faceSize = 20;
-  
-  /**
-   * Used to control the size of the border between cells in the cell matrix
-   */
-  static int cellMatrixBorderSize = 0;
-  
-  /**
-   * Grid JPanel used to represent a Screen object graphically
-   */
-  javax.swing.JPanel cellsMatrix;
-  
-  /**
    * Used to enable full-screen mode
    */
   static GraphicsDevice device = GraphicsEnvironment
         .getLocalGraphicsEnvironment().getScreenDevices()[0];
+  
+  /**
+   * Defines a JPanel subclass that represents a grid of text fields, referred to as cells; the grid itself 
+   * is referred to as the cellsMatrix. The purpose of the cellsMatrix is to emulate the look and feel of a 
+   * console, whilst also allowing the individual cells/characters to be edited.
+   * 
+   * <p>The cellsMatrix resides in a cellsMatrixContainer, a JPanel with a FlowLayout manager that allows 
+   *    the cellsMatrix to be sized by the size of its contents. This container serves further use, such as 
+   *    controlling the border color of the cellsMatrix, and is therefore stored as a member  of cellsMatrix.
+   * 
+   * <p>The CellsMatrix has two modes, which are defined as constructors; these modes are edit mode, and view mode.
+   *    Edit mode is designed for the editing of a screen, while view mode is designed for the viewing of a screen
+   *    as it will look in the console. 
+   */
+  class CellsMatrix extends javax.swing.JPanel {
+    /**
+     * Specifies number of columns of cells in cellsMatrix. Can't be negative or zero, and has a default value
+     * of 10.
+     */
+    private int columnCount = 10;
+    
+    /**
+     * Specifies number of rows of cells in cellsMatrix. Can't be negative or zero, and has a default value
+     * of 10.
+     */
+    private int rowCount = 10;
+    
+    /**
+     * Specifies thickness of border between cells, and between cellsMatrix and 
+     * cellsContainer (acts as sides/top/bottom border). Has a default value of 3.
+     * 
+     * The "border" referred to is actually just the horizontal and vertical gap between the cells 
+     * in cellsMatrix, as well as the margin between cellsMatrix and its container; this space is 
+     * specified by borderThickness. Therefore, the border's color is set by changing the color of 
+     * the cellsMatrixContainer; the container is opaque, while the cellsMatrix is translucent.
+     */
+    private int borderThickness = 3;
+    
+    /**
+     * Specifies the font size of the cells (text fields) in cellsMatrix. Has a default value of 20.
+     */
+    private int fontSize = 20;
+    
+    /**
+     * Specifies the name of the font to be used.
+     */
+    private String fontName = "DejaVu Sans Mono";
+    
+    /**
+     * Contains the font to be applied to the cells in the cellsMatrix.
+     */
+    private Font font;
+    
+    /**
+     * Parent of the cellsMatrix, and allows the cellsMatrix to be sized by it's contents. 
+     */
+    private javax.swing.JPanel cellsMatrixContainer;
+    
+    /**
+     * Edit mode.
+     * 
+     * This constructor initializes the cellsMatrix in edit mode. Edit mode is the intended mode for 
+     * editing the cellsMatrix due to three factors. One factor is the addition of borders between the cells,
+     * as well as between the cellsMatrix and its container; this change allows for a clear separation 
+     * between cells. Another factor is the widening of the cells. This allows for more space inside the 
+     * cells themselves so the characters they contain are more legible. The final change is the 
+     * implementation of a constant, large font size for increased legibility. 
+     * 
+     * @param columnCount initializes columnCount member
+     * @param rowCount initializes rowCount member
+     * @param borderThickness initializes borderThickness member
+     */
+    public CellsMatrix( int columnCount, int rowCount )
+    {
+      this.columnCount = columnCount;
+      this.rowCount = rowCount;
+      
+      cellsMatrixContainer = new javax.swing.JPanel();
+      cellsMatrixContainer.setLayout( new java.awt.FlowLayout( java.awt.FlowLayout.CENTER, 0, 
+        borderThickness // Vertical gap is required in order to get margin above cellsMatrix, which 
+                        // is a problem that doesn't exist for the horizontal gap
+      ) );
+      
+      this.setLayout( new java.awt.GridLayout( rowCount, columnCount, borderThickness, borderThickness ) );
+s
+        // fills cellsMatrix
+      for( int i = 1; i <= rowCount * columnCount; i++ )
+      { 
+        try{
+          char[] toFillWith = new char[]{ ' ','c','e','o','f','y','e','a','s','t','@','L','A','P','T','O','P'};
+          this.add( initializeCell( toFillWith[i] ) );
+        } catch( Exception  e ){
+          this.add( initializeCell( ' ' ) );
+        }
+      }
+
+      cellsMatrixContainer.add( this );
+    };
+    
+    /**
+     * View Mode.
+     * 
+     * This constructor initializes the cellsMatrix in view mode. View mode is the intended mode for viewing a 
+     * representation of screensMatrix as it will appear in the console; this is accomplished using three tricks.
+     * For one, the width and height of the cells are set to the max width and height of a character from the font 
+     * being used. Seeing as only mono-spaced fonts are allowed, and that no more or less than one character can 
+     * exist in a cell, this ensures that the cells are packed as tightly as possible. This reflects the behavior
+     * of a console, where the cells in the console are each the size of one mono-spaced character. The second
+     * way is the setting of the font size by the user. This ensures that the cellsMatrix replicates the user's
+     * console environment as thoroughly as possible. The third and final way is the absence of borders between
+     * the cells. This also serves to ensure that the cells are packed together.
+     * 
+     * @param columnCount initializes columnCount member
+     * @param rowCount initializes rowCount member
+     * @param faceSize initializes faceSize member
+     */
+    public CellsMatrix( int columnCount, int rowCount, int fontSize )
+    {
+      this.columnCount = columnCount;
+      this.rowCount = rowCount;
+      this.fontSize = fontSize;
+      
+      cellsMatrixContainer = new javax.swing.JPanel();
+      cellsMatrixContainer.setLayout( new java.awt.FlowLayout( java.awt.FlowLayout.CENTER, 0, 0 ) );
+      
+      this.setLayout( new java.awt.GridLayout( rowCount, columnCount, 0, 0 ) );
+s
+        // fills cellsMatrix
+      for( int i = 1; i <= rowCount * columnCount; i++ )
+      { 
+        try{
+          char[] toFillWith = new char[]{ ' ','c','e','o','f','y','e','a','s','t','@','L','A','P','T','O','P'};
+          this.add( initializeCell( toFillWith[i] ) );
+        } catch( Exception  e ){
+          this.add( initializeCell( ' ' ) );
+        }
+      }
+
+      cellsMatrixContainer.add( this );
+    }
+    
+    /**
+     * Returns cellsMatrixContainer. It's possible to get the cellsMatrixContainer using cellsMatrix.getParent(), 
+     * but using this method makes it more clear what action is being taken.
+     * 
+     * @return the cellsMatrixContainer
+     */
+    public javax.swing.JPanel getCellsMatrixContainer()
+    {
+      return cellsMatrixContainer;
+    }
+    
+    /**
+     * Initializes font using the fontName and fontSize member variables
+     */
+    public void initializeFont()
+    {
+      this.font = new Font( fontName, Font.PLAIN, fontSize );
+    }
+  }
+  
+  /**
+   * Defines a JTextField subclass that represents a cell in the cellsMatrix. The cellsMatrix is an editable
+   * representation of the console, which means that the cells inside the cellsMatrix are editable representations 
+   * of the "console cells" in a console. Therefore, to understand the functionality of the cell object, one must
+   * understand the functionality of the "console cell". 
+   * 
+   * <p>Every console cell inside the console is the same size and can contain, at most, a single character of the 
+   *    same font. If a console cell doesn't contain a character, it's sized as if it does. Since the console can 
+   *    only use mono-spaced fonts, the console cells' widths and heights are determined by the max width and height 
+   *    of a character from the font being used because the characters are all the same size. The console cells are 
+   *    then scaled up or down based on the font size.
+   *    
+   * <p>The Cell class seeks to replicate all of these behaviors. The individual cells are sized automatically by 
+   *    java swing, as long as they're initialized containing a single character from a mono-spaced font with zero 
+   *    margin.
+   */
+  class Cell extends javax.swing.JTextField {
+    /**
+     * Constructs a cell using a given fontSize and cellFont. 
+     * 
+     * @param fontSize specifies the font size of the cell being constructed
+     * @param cellFont specifies the font of the cell being constructed
+     */
+    public Cell( int fontSize, Font cellFont )
+    {
+      
+    }
+  }
   
   /**
    * Creates new form ScreenEditorTesting
@@ -113,27 +276,6 @@ public class ScreenEditorTesting extends javax.swing.JFrame {
   }
   
   public void initializeCellsMatrix() {
-      // Code block initializes an outer panel to contain the cellsMatrix panel. The Flow layout manager allows for the inner panel to be sized by the size of its contents, despite being a grid.
-    javax.swing.JPanel cellsMatrixContainer = new javax.swing.JPanel();
-    cellsMatrixContainer.setLayout( new java.awt.FlowLayout( java.awt.FlowLayout.CENTER, 0, cellMatrixBorderSize ) );
-    
-    cellsMatrix = new javax.swing.JPanel();
-    
-    cellsMatrix.setLayout( new java.awt.GridLayout( rowCount, columnCount, cellMatrixBorderSize, cellMatrixBorderSize ) );
-    
-    for( int i = 1; i <= rowCount * columnCount; i++ )
-    { 
-      try{
-        char[] toFillWith = new char[]{ ' ','c','e','o','f','y','e','a','s','t','@','L','A','P','T','O','P'};
-        cellsMatrix.add( initializeCell( toFillWith[i] ) );
-      } catch( Exception  e ){
-        cellsMatrix.add( initializeCell( ' ' ) );
-      }
-    }
-    
-    cellsMatrixContainer.add( cellsMatrix );
- 
-    getContentPane().add( cellsMatrixContainer );
   }
   
   public javax.swing.JTextField initializeCell( char cellText ){
@@ -157,7 +299,7 @@ public class ScreenEditorTesting extends javax.swing.JFrame {
   }
   
   public Font initializeFont(){
-    Font toReturn = new Font( "DejaVu Sans Mono", Font.PLAIN, faceSize ); // Initializes the font
+    Font toReturn =  // Initializes the font
     
     //Map< AttributedCharacterIterator.Attribute, String > toReturnAttributes = new HashMap<>(); // Contains extra attributes to add to font
     
